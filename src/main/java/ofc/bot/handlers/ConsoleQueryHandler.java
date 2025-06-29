309e020033efdb8001f350a535f5ceb61cc14b9c
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-06-29 16:24:04 -0300

    Make the console query handler print JSON instead of tables

--- src/main/java/ofc/bot/handlers/ConsoleQueryHandler.java
+++ src/main/java/ofc/bot/handlers/ConsoleQueryHandler.java
@@ -1,8 +1,8 @@
 package ofc.bot.handlers;
 
 import ofc.bot.domain.sqlite.DB;
-import org.jooq.DSLContext;
-import org.jooq.Result;
+import org.jooq.*;
+import org.jooq.Record;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,11 +14,11 @@ import java.util.concurrent.RejectedExecutionException;
 public class ConsoleQueryHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConsoleQueryHandler.class);
     private static final ExecutorService EXECUTOR = Executors.newSingleThreadExecutor();
+    private static final JSONFormat DEFAULT_JSON_FORMAT = new JSONFormat().recordFormat(JSONFormat.RecordFormat.OBJECT);
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final DSLContext CTX = DB.getContext();
 
     public static void init() {
-
         try {
             EXECUTOR.execute(ConsoleQueryHandler::run);
             LOGGER.info("Successfully initialized ConsoleQueryHandler thread");
@@ -28,50 +28,33 @@ public class ConsoleQueryHandler {
     }
 
     private static void run() {
-
         while (true) {
-
             String sql = SCANNER.nextLine();
 
             if (sql == null || sql.isBlank()) {
                 LOGGER.warn("SQL queries may not be empty");
                 continue;
             }
-
             executeAndPrettyPrint(sql);
         }
     }
 
-    private static void executeAndPrettyPrint(String inputSQL) {
-
-        String upperSQL = inputSQL.toUpperCase();
-
-        if (upperSQL.contains("DELETE") && !upperSQL.contains("WHERE")) {
-            LOGGER.warn("The console command sender is not allwed to DELETE without a WHERE condition");
-            return;
-        }
-
-        if (upperSQL.contains("DROP") && !upperSQL.startsWith("!")) {
-            LOGGER.warn("The console command sender is not allowed to perform DROP operations without a validation");
-            return;
-        }
-
+    private static void executeAndPrettyPrint(String sql) {
         try {
+            Result<Record> res = CTX.fetch(sql);
+            int size = res.size();
 
-            String SQL = inputSQL.startsWith("!")
-                    ? inputSQL.substring(1)
-                    : inputSQL;
-
-            if (SQL.trim().startsWith("?")) {
-
-                Result<?> result = CTX.fetch(SQL.substring(1));
-                LOGGER.info("Fetch Output:\n{}", result.format());
-            } else {
-
-                int affectedRows = CTX.execute(SQL);
-                LOGGER.info("Operation successful, {} rows affected.", affectedRows);
+            if (size == 0) {
+                LOGGER.warn("No results found");
+                return;
             }
 
+            if (size == 1) {
+                Record rec = res.getFirst();
+                LOGGER.info("Result:\n{}", rec.formatJSON(DEFAULT_JSON_FORMAT));
+            } else {
+                LOGGER.info("Results:\n{}", res.formatJSON(DEFAULT_JSON_FORMAT));
+            }
         } catch (Exception e) {
             LOGGER.error("Could not execute SQL command because: {}", e.getMessage());
         }
