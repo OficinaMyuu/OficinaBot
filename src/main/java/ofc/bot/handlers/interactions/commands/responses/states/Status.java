5eb836e25860a64c1f92956cda565ccb9089deeb
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-10-19 18:14:05 -0300

    Add rejected ban command message at `Status.java` [skip ci]

--- src/main/java/ofc/bot/handlers/interactions/commands/responses/states/Status.java
+++ src/main/java/ofc/bot/handlers/interactions/commands/responses/states/Status.java
@@ -183,6 +183,7 @@ public enum Status implements InteractionResult {
     GROUP_ALREADY_HAS_THE_PROVIDED_CHANNEL(     State.FAILURE, true,  "O grupo já possui o tipo de canal fornecido."),
     NAME_TOO_SHORT(                             State.FAILURE, true,  "Nomes precisam ter %d ou mais caracteres."),
     YOU_CANNOT_PUNISH_YOURSELF(                 State.FAILURE, true,  "Você não pode punir você mesmo."),
+    YOU_CANNOT_BAN_YOURSELF(                    State.FAILURE, true,  "Você não pode banir você mesmo."),
     YOUR_GROUP_DOES_NOT_HAVE_TEXT_CHANNEL(      State.FAILURE, true,  "Seu grupo não possui um canal de texto."),
     FAILED_TO_VALIDATE_PASSPHRASE(              State.FAILURE, true,  "Não foi possível validar a frase de confirmação."),
     INCORRECT_CONFIRMATION_VALUE(               State.FAILURE, true,  "O valor de confirmação está incorreto."),
