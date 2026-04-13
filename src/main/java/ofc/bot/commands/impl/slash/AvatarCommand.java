4a79bc7766230592d2fb0bb1f77991ddbe16f5ba
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-04-12 23:27:12 -0300

    Make `/avatar` command not download the image and resend it

--- src/main/java/ofc/bot/commands/impl/slash/AvatarCommand.java
+++ src/main/java/ofc/bot/commands/impl/slash/AvatarCommand.java
@@ -1,12 +1,13 @@
 package ofc.bot.commands.impl.slash;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.api.utils.FileUpload;
-import net.dv8tion.jda.api.utils.ImageProxy;
 import ofc.bot.handlers.interactions.commands.contexts.impl.SlashCommandContext;
 import ofc.bot.handlers.interactions.commands.responses.states.InteractionResult;
 import ofc.bot.handlers.interactions.commands.responses.states.Status;
@@ -14,62 +15,21 @@ import ofc.bot.handlers.interactions.commands.slash.abstractions.SlashCommand;
 import ofc.bot.util.Bot;
 import ofc.bot.util.content.annotations.commands.DiscordCommand;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.awt.*;
-import java.io.InputStream;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 @DiscordCommand(name = "avatar")
 public class AvatarCommand extends SlashCommand {
-    private static final Logger LOGGER = LoggerFactory.getLogger(AvatarCommand.class);
 
     @Override
     public InteractionResult onCommand(@NotNull SlashCommandContext ctx) {
-        MessageEmbed embed;
-        FileUpload upload;
-        Member issuer = ctx.getIssuer();
-        User user = ctx.getUser();
-        Guild guild = ctx.getGuild();
-        boolean fromGuild = ctx.getOption("local", false, OptionMapping::getAsBoolean);
-        boolean targetProvided = ctx.hasOption("user");
+        boolean localAvatarRequested = ctx.getOption("local", false, OptionMapping::getAsBoolean);
 
-        if (fromGuild) {
-            Member targetInput = ctx.getOption("user", OptionMapping::getAsMember);
-            Member target = targetProvided ? targetInput : issuer;
-
-            if (target == null)
-                return Status.MEMBER_NOT_IN_GUILD;
-
-            ImageProxy avatar = target.getAvatar();
-            String avatarId = target.getAvatarId();
-            String fileName = String.format("%s.%s", avatarId, isAnimated(avatarId) ? "gif" : "png");
-
-            if (avatar == null)
-                return Status.NO_GUILD_AVATAR_PRESENT;
-
-            upload = FileUpload.fromData(downloadAvatar(avatar), fileName);
-
-            embed = embed(upload, guild, target.getUser());
-        } else {
-            User target = ctx.getOption("user", user, OptionMapping::getAsUser);
-            ImageProxy avatar = target.getEffectiveAvatar();
-            String avatarId = target.getAvatarId();
-            String fileName = String.format("%s.%s", avatarId, isAnimated(avatarId) ? "gif" : "png");
-
-            upload = FileUpload.fromData(downloadAvatar(avatar), fileName);
-
-            embed = embed(upload, guild, target);
+        if (localAvatarRequested) {
+            return replyWithGuildAvatar(ctx);
         }
 
-        // We do not use a try with resources because JDA's Requester closes
-        // the stream automatically once it's sent.
-        return ctx.create()
-                .setEmbeds(embed)
-                .setFiles(upload)
-                .send();
+        return replyWithGlobalAvatar(ctx);
     }
 
     @NotNull
@@ -87,35 +47,47 @@ public class AvatarCommand extends SlashCommand {
         );
     }
 
-    private boolean isAnimated(String url) {
-        return url != null && url.startsWith("a_");
-    }
+    private InteractionResult replyWithGuildAvatar(SlashCommandContext ctx) {
+        Member target = resolveGuildTarget(ctx);
 
-    private InputStream downloadAvatar(ImageProxy img) {
-
-        try {
-            return img.download(2048).get();
-        } catch (InterruptedException | ExecutionException e) {
-
-            LOGGER.warn("Could not download avatar at \"{}\"", img.getUrl(), e);
-
-            // I mean... what else can we do about it?
-            return InputStream.nullInputStream();
+        if (target == null) {
+            return Status.MEMBER_NOT_IN_GUILD;
         }
+
+        String avatarUrl = target.getAvatarUrl();
+        if (avatarUrl == null) {
+            return Status.NO_GUILD_AVATAR_PRESENT;
+        }
+
+        MessageEmbed embed = buildAvatarEmbed(ctx.getGuild(), target.getUser(), avatarUrl);
+        return ctx.replyEmbeds(embed);
     }
 
-    private MessageEmbed embed(FileUpload file, Guild guild, User target) {
+    private InteractionResult replyWithGlobalAvatar(SlashCommandContext ctx) {
+        User target = ctx.getOption("user", ctx.getUser(), OptionMapping::getAsUser);
+        String avatarUrl = target.getEffectiveAvatarUrl();
+        MessageEmbed embed = buildAvatarEmbed(ctx.getGuild(), target, avatarUrl);
 
-        EmbedBuilder builder = new EmbedBuilder();
-        String name = target.getName();
-        String title = "🖼 " + name;
-        Color color = Bot.Colors.DISCORD;
+        return ctx.replyEmbeds(embed);
+    }
 
-        return builder
-                .setTitle(title, target.getEffectiveAvatarUrl())
-                .setDescription(String.format("Avatar de `%s`", target.getEffectiveName()))
-                .setColor(color)
-                .setImage("attachment://" + file.getName())
+    private Member resolveGuildTarget(SlashCommandContext ctx) {
+        if (ctx.hasOption("user")) {
+            return ctx.getOption("user", OptionMapping::getAsMember);
+        }
+
+        return ctx.getIssuer();
+    }
+
+    private MessageEmbed buildAvatarEmbed(Guild guild, User target, String avatarUrl) {
+        String title = "\uD83D\uDDBC " + target.getName();
+        String description = String.format("Avatar de `%s`", target.getEffectiveName());
+
+        return new EmbedBuilder()
+                .setTitle(title, avatarUrl + "?size=2048")
+                .setDescription(description)
+                .setColor(Bot.Colors.DISCORD)
+                .setImage(avatarUrl)
                 .setFooter(guild.getName(), guild.getIconUrl())
                 .build();
     }
