a1bdfcd3cbe882134f7299c8f7e7e52b519da256
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-07-12 02:41:48 -0300

    Remove dashboard header `guild` element

--- frontend/src/components/layout/DashboardHeader.tsx
+++ frontend/src/components/layout/DashboardHeader.tsx
@@ -1,5 +1,5 @@
 import { FiMenu } from "react-icons/fi"
-import { useSession } from "@/contexts/SessionContext"
+
 import styles from "./DashboardHeader.module.css"
 
 type DashboardHeaderProps = {
@@ -7,15 +7,12 @@ type DashboardHeaderProps = {
 }
 
 export function DashboardHeader({ title }: DashboardHeaderProps) {
-  const { user } = useSession()
-
   return (
     <header className={styles.header}>
       <div className={styles.title}>
         <FiMenu aria-hidden="true" />
         <h1>{title}</h1>
       </div>
-      <span className={styles.guild}>{user?.guild_name ?? "Oficina"}</span>
     </header>
   )
 }
